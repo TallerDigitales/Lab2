14d7d7c38e482b0704a291f016202cc65b102e2e
crisrivlop <crisrivlop@gmail.com>
2018-08-15 13:47:21 +0000

    counter: structural model + selfchecked testbench

--- counter.sv
+++ counter.sv
@@ -1,12 +1,24 @@
 module counter #(parameter bus = 4)
 	(input logic clk,input logic reset, output logic[bus-1:0] q);
 	logic[bus-1:0] d;
-	assign d[bus-1:0] = '0;
+	logic carryout,cin;
+	adder #(bus) _adder(d,0,cin,q,carryout);
 	always_ff@(posedge clk)
 		begin
-			if(q[bus-1:0] === '1 || q[bus-1:0] === 'X || reset) 
-				q[bus-1:0] <= '0;
+			if(q[bus-1:0] === 'X || reset) 
+				begin
+					d[bus-1:0] <= '1;
+					cin <=1;
+				end
+			else if (carryout)
+				begin
+					d[bus-1:0] <= '0;
+					cin <=1;
+				end
 			else 
-				q <= q + 1'b1;
+				begin 
+					d <= q;
+					cin <= 1;
+				end
 		end
 endmodule
--- counter_tb.sv
+++ counter_tb.sv
@@ -6,17 +6,77 @@ module counter_tb ();
 	counter #(2) counter1(clk,reset,sout1);
 	counter #(3) counter2(clk,reset,sout2);
 	counter #(4) counter3(clk,reset,sout3);
-	
+	/*
 	always
 		begin
 			clk = 1; #10; clk = 0; #10;
 		end
-		
+	*/	
 	initial begin
 		reset = 1;
+		clk = 1;
 		#10;
+		
+		if(sout1 != 2'b00) $display("Error: At first clock cycle output isn't 00");
+		if(sout2 != 3'b000) $display("Error: At first clock cycle output isn't 000");
+		if(sout3 != 4'b0000) $display("Error: At first clock cycle output isn't 0000");
+		
 		reset = 0;
+		clk = 0;
 		#10;
+		
+		
+		clk = 1;
+		#10;
+		
+		if(sout1 != 2'b01) 
+			$display("Error: At second clock cycle output isn't 01");
+		if(sout2 != 3'b001) 
+			$display("Error: At second clock cycle output isn't 001");
+		if(sout3 != 4'b0001) 
+			$display("Error: At second clock cycle output isn't 0001");
+		
+		clk = 0;
+		#10;
+		
+		
+		clk = 1;
+		#10;
+		
+		if(sout1 != 2'b10) $display("Error: At third clock cycle output isn't 10");
+		if(sout2 != 3'b010) $display("Error: At third clock cycle output isn't 010");
+		if(sout3 != 4'b0010) $display("Error: At third clock cycle output isn't 0010");
+				
+		
+		clk = 0;
+		#10;
+		
+		
+		clk = 1;
+		#10;
+		
+		
+		if(sout1 != 2'b11) $display("Error: At forth clock cycle output isn't 11");
+		if(sout2 != 3'b011) $display("Error: At forth clock cycle output isn't 011");
+		if(sout3 != 4'b0011) $display("Error: At forth clock cycle output isn't 0011");
+		
+
+		clk = 0;
+		#10;
+		
+		
+		clk = 1;
+		#10;
+		
+		
+		if(sout1 != 2'b00) $display("Error: Clock cycle output isn't 00");
+		if(sout2 != 3'b100) $display("Error: Clock cycle output isn't 100");
+		if(sout3 != 4'b0100) $display("Error: Clock cycle output isn't 0100");
+		
+
+		clk = 0;
+		#10;
+		
 	
 	end
 
